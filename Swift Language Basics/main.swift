b7c3f25c3cca81e509d6efa5e3eb4ad74a0f283e
ylkwd <yaoxuanluan@lyxs-15-Pro.local>
2017-09-08 19:54:54 -0500

    First

---
+++ Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Yaoxuan Luan on 9/8/17.
+//  Copyright © 2017 Yaoxuan Luan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
